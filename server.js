cf1cd07885625d3f230157d26c78ad2729c017c9
Chase Thomason <cvthomason@gmail.com>
2014-06-02 15:24:47 -0600

    added more text

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+// Does stuff
+/* long comment
+uihhiuiuhhiuihu
+
+
+
+iiuinuuinniu
+what up */
+
 var http = require('http'),
 	os = require ('os'),
 	port = process.env.PORT || 8000;
@@ -6,7 +15,7 @@ http.createServer(function (request, response) {
 	response.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
-	response.end('Hello World\n');
+	response.end('Hello World peace be unto you\n');
 }).listen(port);
 
-console.log('Server is running at http;//' + os.hostname() + ":" + port);
+console.log('Server is running at http://' + os.hostname() + ":" + port);
